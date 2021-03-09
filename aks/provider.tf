c99b04683f7a198c3f189bddc0dd48aa3b76365f
Igor Arruda <igor.arruda@hp.com>
2021-03-09 20:46:30 -0300

    fix formatation

--- aks/provider.tf
+++ aks/provider.tf
@@ -6,7 +6,7 @@ terraform {
 # Configure the Azure Provider
 provider "azurerm" {
   # It is recommended to pin to a given version of the Provider
-  version = "=2.47.0"
+  version         = "=2.47.0"
   subscription_id = var.subscription_id
   client_id       = var.client_id
   tenant_id       = var.tenant_id
