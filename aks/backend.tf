a5c5636735def2ab46b5824e4722ca1a5d1e5ca2
Igor Arruda <igor.arruda@hp.com>
2021-03-09 20:28:44 -0300

    fix backend remote

--- aks/backend.tf
+++ aks/backend.tf
@@ -1,14 +1,5 @@
 #Set the terraform backend
 terraform {
-  # Backend variables are initialized by Azure DevOps
-  backend "azurerm" {
-    resource_group_name  = "cloud-shell-storage-eastus"
-    storage_account_name = "cs210030000ac9af3b2"
-    container_name       = "terraform-state"
-    key                  = "prod.terraform.tfstate.kb8"
-    access_key           = "HJcpl2/tXOQJnoWbNGhh42uJV9fXVgMvpa8H01cB11Z1J1ZS2kdrOgclAlJiEyfgOfw71LSmqrHjsDfzw0/BHQ=="
-  }
-
   backend "remote" {
     organization = "eesdevops"
 
